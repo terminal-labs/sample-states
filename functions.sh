b9ffb3d4eadd9eaa8360edfce44f3d83b3ac0d46
Michael Verhulst <michael@terminallabs.com>
2018-03-30 13:26:04 -0500

    Delete functions.sh

--- functions.sh
+++
@@ -1,9 +0,0 @@
-function saltmaster { # $1 = location, e.g. 'master' or '*', $2 = command
-    start="su saltmaster -c \"cd /home/saltmaster;\
-     source salt_venv/bin/activate;\
-     python /home/saltmaster/salt_src/scripts/salt '"
-    middle="' -c /home/saltmaster/salt_controlplane/etc/salt "
-    end=" \""
-    command=$start$1$middle$2$end
-    eval $command
-}
