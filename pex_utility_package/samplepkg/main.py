641f2db78ddb7273b559cbd55bf383815130e59c
Michael Verhulst <michael@terminallabs.com>
2018-04-03 13:50:49 -0500

    Update main.py

--- pex_utility_package/samplepkg/main.py
+++ pex_utility_package/samplepkg/main.py
@@ -1,3 +1,11 @@
 # requests isn't actually used, but this demonstrates it being available in main.py
 import requests
-print("HELLO WORLD!!")
+import click
+
+@click.command()
+def hello(count, name):
+    """Simple program that greets with hello"""
+    click.echo('Hello')
+
+if __name__ == '__main__':
+    hello()
