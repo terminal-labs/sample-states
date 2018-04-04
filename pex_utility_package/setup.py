bf548dd09881444167971329c729b8d7ab73cca9
Michael Verhulst <michael@terminallabs.com>
2018-04-04 01:14:35 -0500

    Update setup.py

--- pex_utility_package/setup.py
+++ pex_utility_package/setup.py
@@ -1,9 +1,5 @@
 from setuptools import setup
 setup (
   name = "myexample",
-  packages=['samplepkg'],
-  entry_points='''
-    [console_scripts]
-    tool=samplepkg.main:main
-  '''
+  packages=['samplepkg']
 )
