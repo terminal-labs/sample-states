6971575d04bd4cd12ea2f6407825779dd608c4e2
Michael Verhulst <michael@terminallabs.com>
2018-04-04 22:45:30 -0500

    Update main.py

--- pex_utility_package/samplepkg/main.py
+++ pex_utility_package/samplepkg/main.py
@@ -15,7 +15,8 @@ def truncate_minion_key_names():
         for filename in filenames:
             if filename.count('-') == 2:
                 os.rename(path + '/' + filename, path + '/' + filename.split('-')[0] + '-' + filename.split('-')[1] )
-    return "truncated some key names"
+        return "truncated some key names"
+    return "minion key path not found"
 
 if __name__ == "__main__":
     cli()
