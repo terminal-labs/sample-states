d6e576b4c0f35e792e64c5aadee16dd6e8f9ad18
Michael Verhulst <michael@terminallabs.com>
2018-04-05 00:21:44 -0500

    Update main.py

--- pex_utility_package/samplepkg/main.py
+++ pex_utility_package/samplepkg/main.py
@@ -8,6 +8,27 @@ def cli(ctx):
     pass
 
 @cli.command()
+def truncate_minion_id():
+    file_in = open('/etc/salt/minion')
+    lines = file_in.readlines()
+    file_in.seek(0)
+    content = file_in.read()
+    file_in.close()
+    
+    for line in lines:
+        if ":" in line:
+            split_line = line.split(":")
+            if 'id' in split_line[0]:
+                old_id = split_line[1].strip()
+                new_id = old_id.split('-')[0] + '-' + old_id.split('-')[1]
+                old_id_line = line
+                new_id_line =  split_line[0] + ": " +  new_id + "\n"
+                new_content = content.replace(old_id_line, new_id_line)
+                file_in = open('/etc/salt/minion', 'w')
+                file_in.write(new_content)
+                file_in.close()
+
+@cli.command()
 def truncate_minion_key_names():
     path = '/home/saltmaster/salt_master_root/etc/salt/pki/master/minions'
     if os.path.exists(path):
